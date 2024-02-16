ffca3498759a0af2378f76eb534ef70215727dcb
yuvraj-05828 <yuvraj05828@gmail.com>
2024-02-16 22:23:34 +0530

    updated

---
+++ react/src/App.jsx
@@ -0,0 +1,13 @@
+import Calculator from './components/calc';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    <Calculator/>
+    </div>
+  );
+}
+
+export default App;
